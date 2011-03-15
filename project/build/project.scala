bfa8037602721ba724be01463d45264800184973
Nathan Hamblen <nathan@technically.us>
2011-03-15 02:20:24 -0400

    dep is now in harness

--- project/build/project.scala
+++ project/build/project.scala
@@ -3,6 +3,4 @@ import sbt._
 class MyProject(info: ProjectInfo) 
     extends DefaultProject(info)
     with conscript.Harness {
-  val launchInterface = 
-    "org.scala-tools.sbt" % "launcher-interface" % "0.7.4" % "provided"
 }
