50f535eff2dc3433ceaf8c8d891ddfd82cf39fac
Nathan Hamblen <nathan@technically.us>
2011-03-15 02:16:42 -0400

    using conscript harness plugin

---
+++ src/main/scala/App.scala
@@ -0,0 +1,9 @@
+package screpl
+
+class App extends xsbti.AppMain {
+  def run(config: xsbti.AppConfiguration) = {
+    println("Hello World: " + config.arguments.mkString(" "))
+    Exit(0)
+  }
+}
+case class Exit(val code: Int) extends xsbti.Exit
